d452104af1297e603e595a9e54d6c6ce0527c5cc
Default Seadas User <seadas-user@localhost>
2016-01-23 17:31:24 -0500

    Comments added...

--- Research/LANDSAT8/doc_algorithm_ag412_daily.m
+++ Research/LANDSAT8/doc_algorithm_ag412_daily.m
@@ -1,8 +1,21 @@
-function [ DOC ag412 ] = doc_algorithm_ag412_daily( Rrs443, Rrs547, jd )
-% Revised coefficients from Antonio Mannino on October 3, 2014
+function [DOC,ag412] = doc_algorithm_ag412_daily( Rrs443, Rrs547, jd )
+% Function to determine DOC and ag412 from MODIS Aqua Rrs @ 443 and 547nm
+% Inputs:
+% - Rrs443: remote-sensing reflectance @ 443nm
+% - Rrs547: remote-sensing reflectance @ 547nm
+% - jd: Julian day
+% Outputs:
+% - DOC: Dissolved Organic Carbon concentration
+% - ag412: absorption of Gelbstoff or CDOM absorption @ 412 nm
+% by Sergio R. Signorini, Ph. D.
+% GSFC - NASA
+% Modified by Javier A. Concha, Ph.D.
+% 2016-01-23
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Revised coefficients from Antonio Mannino on October 3, 2014
 % cf=0 for sigmmoidal function, cf=1 for linear function
 cf=1;
-% A. Mannino DOC algorithm
+%% A. Mannino DOC algorithm
 Y=-3.070-1.285.*log(Rrs443)+1.107.*log(Rrs547);
 ag412=exp(Y);
 % DOC algorithm coefficients: Fall-Winter-Spring & Summer
@@ -10,17 +23,17 @@ ag412=exp(Y);
 %B1= 0.0033968; B2= 0.0047885;
 A1=-0.0046493; A2=-0.0026127;
 B1= 0.003305; B2= 0.003771;
-% Transition 1) May 16 to June 15 and 2) Oct 1 to Oct 31
+%% Transition 1) May 16 to June 15 and 2) Oct 1 to Oct 31
 % 136:166 & 274:304
 % June 16 to Sept 30 - Summer
 DOC1=1./(log(ag412).*A1 + B1); % Fall-Winter-Spring
 DOC2=1./(log(ag412).*A2 + B2); % Summer
 if ( jd >= 167 && jd <= 273) % Summer
     DOC=DOC2;
-% Nov 1 to May 15 - Fall-Winter-Spring
+    % Nov 1 to May 15 - Fall-Winter-Spring
 elseif (jd>= 305 || (jd >=1 && jd <= 135))
     DOC=DOC1;
-end
+end %fi
 if( jd >= 136 && jd <= 166) % May 16 to June 15
     % Sigmoidal weights for blending the two algorithms
     if (cf==0)
@@ -34,15 +47,15 @@ if( jd >= 136 && jd <= 166) % May 16 to June 15
         W1=S(id);
         W2=W(id);
     elseif (cf==1)
-        d1=136; 
+        d1=136;
         d2=166;
-        a=1/(d2-d1); 
+        a=1/(d2-d1);
         b=-d1/(d2-d1);
         W1=a.*jd + b;
         W2=1-W1;
     end
     DOC=W1.*DOC2 + W2.*DOC1;
-end
+end % fi
 if( jd >= 274 && jd <= 304) % October 1-31
     % Sigmoidal weights for blending the two algorithms
     if(cf == 0)
@@ -56,20 +69,21 @@ if( jd >= 274 && jd <= 304) % October 1-31
         W1=S(id);
         W2=W(id);
     elseif (cf==1)
-        d1=274; 
+        d1=274;
         d2=304;
-        a=1/(d2-d1); 
+        a=1/(d2-d1);
         b=-d1/(d2-d1);
         W1=a.*jd + b;
         W2=1-W1;
-    end
+    end % fi
     DOC=W1.*DOC1 + W2.*DOC2;
-end
+end % fi
+%%
 DOC=real(DOC);
 DOC=DOC*12.01; % umol/L to mg/m3
 id=DOC<0 | DOC>3000;
 %id=DOC<0;
 DOC(id)=NaN;
 ag412(id)=NaN;
-end
+% end % noitcnuf
 
