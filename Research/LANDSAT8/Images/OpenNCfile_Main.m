3ff468a4ae595696c79bcc6c4020da423826ca26
Default Seadas User <seadas-user@localhost>
2016-01-27 12:47:50 -0500

    Run all necessary plots to compare the data

--- Research/LANDSAT8/Images/OpenNCfile_Main.m
+++ Research/LANDSAT8/Images/OpenNCfile_Main.m
@@ -1,12 +1,12 @@
+%% Load Landsat images
+dirname = '/Users/jconchas/Documents/Research/LANDSAT8/Images/artic_L2wag412/';
+% filename = 'LC80720212014209LGN00';
+filename = 'LC80770102014196LGN00';
+extension = '_L2.nc';
+filepath = [dirname filename extension];
 
-dirname = '/Users/jconchas/Documents/Research/LANDSAT8/Images/LC80720212014209LGN00';
-filename = 'LC80720212014209LGN00_L2test.nc';
-filepath = [dirname '/' filename];
-
-ncdisp(filepath)
-ncinfo(filepath)
-%%
-% Load Landsat images
+% ncdisp(filepath)
+% ncinfo(filepath)
 
 %     infiles = dir('/disk04/Landsat/Processing/Australia/LC8115075*_Australia.L2');  % file has reflectances and Lw,Lt,La for noise model
 
@@ -17,53 +17,72 @@ latitude    = ncread(filepath,'/navigation_data/latitude');
 ag_412_mlrc = ncread(filepath,'/geophysical_data/ag_412_mlrc');
 Rrs443      = ncread(filepath,'/geophysical_data/Rrs_443');
 Rrs561      = ncread(filepath,'/geophysical_data/Rrs_561');
-Lt443       = ncread(filepath,'/geophysical_data/Lt_443');
-Lt561       = ncread(filepath,'/geophysical_data/Lt_561');
-Lt2201      = ncread(filepath,'/geophysical_data/Lt_2201');
-Lw443       = ncread(filepath,'/geophysical_data/Lw_443');
-Lw561       = ncread(filepath,'/geophysical_data/Lw_561');
-La443       = ncread(filepath,'/geophysical_data/La_443');
-La561       = ncread(filepath,'/geophysical_data/La_561');
-La2201      = ncread(filepath,'/geophysical_data/La_2201');
-%%
-chl_oc3     = ncread(filepath,'/geophysical_data/chl_oc3');
+% Lt443       = ncread(filepath,'/geophysical_data/Lt_443');
+% Lt561       = ncread(filepath,'/geophysical_data/Lt_561');
+% Lt2201      = ncread(filepath,'/geophysical_data/Lt_2201');
+% Lw443       = ncread(filepath,'/geophysical_data/Lw_443');
+% Lw561       = ncread(filepath,'/geophysical_data/Lw_561');
+% La443       = ncread(filepath,'/geophysical_data/La_443');
+% La561       = ncread(filepath,'/geophysical_data/La_561');
+% La2201      = ncread(filepath,'/geophysical_data/La_2201');
+% chl_oc3     = ncread(filepath,'/geophysical_data/chl_oc3');
 year        = str2double(filename(10:13));
 julday      = str2double(filename(14:16));
-%     end
 
 % figure,imagesc(ag412)
-%% Plot chl_oc3 from nc from SeaDAS
-plusdegress = 0.5;
-latlimplot = [min(latitude(:))-.5*plusdegress max(latitude(:))+.5*plusdegress];
-lonlimplot = [min(longitude(:))-plusdegress max(longitude(:))+plusdegress];
 
-figure('Color','white')
-% ax = worldmap([52 75],[170 -120]);
-ax = worldmap(latlimplot,lonlimplot);
+clear dirname extension filepath
+%% Checking number of NaN and Negatives
+disp('----------------------------------------------')
+fprintf('Pixels in the Image: %2.8G\n',size(Rrs443(:),1))
+fprintf('NaN values ag_412_mlrc: %2.8G\n',sum(isnan(ag_412_mlrc(:))))
+fprintf('NaN values Rr443: %2.8G\n',sum(isnan(Rrs443(:))))
+fprintf('NaN values Rr561: %2.8G\n',sum(isnan(Rrs561(:))))
 
-load coastlines
-geoshow(ax, coastlat, coastlon,...
-'DisplayType', 'polygon', 'FaceColor', [.45 .60 .30])
+fprintf('Neg. values ag_412_mlrc: %2.8G\n',sum(ag_412_mlrc(:)<0))
+fprintf('Neg. values Rr443: %2.8G\n',sum(Rrs443(:)<0))
+fprintf('Neg. values Rr561: %2.8G\n',sum(Rrs561(:)<0))
 
-geoshow(ax,'worldlakes.shp', 'FaceColor', 'cyan')
-geoshow(ax,'worldrivers.shp', 'Color', 'blue')
-% Display product
+figure('Color','white','Name',filename),histogram(Rrs443(:)),title('Rrs443')
+figure('Color','white','Name',filename),histogram(Rrs561(:)),title('Rrs561')
 
-geoshow(ax,latitude,longitude,log10(chl_oc3),'DisplayType','surface','ZData',zeros(size(chl_oc3)),'CData',log10(chl_oc3))
-colormap jet
-caxis([log10(0.01),log10(20)]) % from colorbar in SeaDAS
-hcb = colorbar('southoutside');
-% set(get(hcb,'Xlabel'),'String','Chl-a [mg m\^-3]')
-fs = 11;
-set(hcb,'fontsize',fs,'Location','southoutside')
-set(hcb,'Position',[.2 .15 .6 .05])
-title(hcb,'chl-a [mg m\^-3]','FontSize',fs)
-set(gca, 'Units', 'normalized', 'Position', [0 0.1 1 1])
+figure('Color','white','Name',filename),imagesc(Rrs443<0),colormap('gray'),title('Rrs443<0')
+figure('Color','white','Name',filename),imagesc(Rrs561<0),colormap('gray'),title('Rrs561<0')
+disp('----------------------------------------------')
+% 
+% %% Plot chl_oc3 from nc from SeaDAS
+% plusdegress = 0.5;
+% latlimplot = [min(latitude(:))-.5*plusdegress max(latitude(:))+.5*plusdegress];
+% lonlimplot = [min(longitude(:))-plusdegress max(longitude(:))+plusdegress];
+% 
+% figure('Color','white','Name',filename)
+% % ax = worldmap([52 75],[170 -120]);
+% ax = worldmap(latlimplot,lonlimplot);
+% 
+% load coastlines
+% geoshow(ax, coastlat, coastlon,...
+% 'DisplayType', 'polygon', 'FaceColor', [.45 .60 .30])
+% 
+% geoshow(ax,'worldlakes.shp', 'FaceColor', 'cyan')
+% geoshow(ax,'worldrivers.shp', 'Color', 'blue')
+% % Display product
+% 
+% geoshow(ax,latitude,longitude,log10(chl_oc3),'DisplayType','surface','ZData',zeros(size(chl_oc3)),'CData',log10(chl_oc3))
+% colormap jet
+% caxis([log10(0.01),log10(20)]) % from colorbar in SeaDAS
+% hcb = colorbar('southoutside');
+% % set(get(hcb,'Xlabel'),'String','Chl-a [mg m\^-3]')
+% fs = 11;
+% set(hcb,'fontsize',fs,'Location','southoutside')
+% set(hcb,'Position',[.2 .15 .6 .05])
+% title('Chl-a (SeaDAS)','FontSize',fs)
+% title(hcb,'Chl-a (mg m\^-3)','FontSize',fs)
+% % set(gca, 'Units', 'normalized', 'Position', [0 0.1 1 1])
 % y = get(hcb,'XTick');
-[xmin,xmax] = caxis;
-x = [10^xmin 0.07 0.45 2.99 10^xmax];
-set(hcb,'XTick',log10(x));
-set(hcb,'XTickLabel',x)
+% % [xmin,xmax] = caxis;
+% x = 10.^y;
+% set(hcb,'XTick',log10(x));
+% set(hcb,'XTickLabel',x)
 
 %% Plot ag_412_mlrc from nc from SeaDAS
 
@@ -71,7 +90,7 @@ plusdegress = 0.5;
 latlimplot = [min(latitude(:))-.5*plusdegress max(latitude(:))+.5*plusdegress];
 lonlimplot = [min(longitude(:))-plusdegress max(longitude(:))+plusdegress];
 
-figure('Color','white')
+figure('Color','white','Name',filename)
 % ax = worldmap([52 75],[170 -120]);
 ax = worldmap(latlimplot,lonlimplot);
 
@@ -85,13 +104,16 @@ geoshow(ax,'worldrivers.shp', 'Color', 'blue')
 
 geoshow(ax,latitude,longitude,log10(ag_412_mlrc),'DisplayType','surface','ZData',zeros(size(ag_412_mlrc)),'CData',log10(ag_412_mlrc))
 colormap jet
-caxis([log10(0.00030006314),log10(2.968400001526)]) % from colorbar in SeaDAS
+cmin = nanmin(ag_412_mlrc(:));
+cmax = nanmax(ag_412_mlrc(:));
+caxis([log10(cmin),log10(cmax)]) % from colorbar in SeaDAS
 hcb = colorbar('southoutside');
 % set(get(hcb,'Xlabel'),'String','Chl-a [mg m\^-3]')
 fs = 11;
 set(hcb,'fontsize',fs,'Location','southoutside')
 set(hcb,'Position',[.2 .15 .6 .05])
-title(hcb,'ag\_412 [m\^-1]','FontSize',fs)
+title('ag\_412\_mlrc (SeaDAS)','FontSize',fs)
+title(hcb,'a_{CDOM} (m\^-1)','FontSize',fs)
 % set(gca, 'Units', 'normalized', 'Position', [0 0.1 1 1])
 y = get(hcb,'XTick');
 % [xmin,xmax] = caxis;
@@ -101,23 +123,23 @@ set(hcb,'XTickLabel',x)
 
 %% From Sergio's script
 [DOC,ag412] = doc_algorithm_ag412_daily( Rrs443, Rrs561, julday );
-%% Quick display
-figure('Color','white')
-imagesc(log10(ag412))
-colormap jet
-caxis([log10(min(ag412(:))),log10(max(ag412(:)))])  
-hcb = colorbar('southoutside');
-y = get(hcb,'XTick');
-% [xmin,xmax] = caxis;
-x = 10.^y;
-set(hcb,'XTick',log10(x));
-set(hcb,'XTickLabel',x)
+% %% Quick display
+% figure('Color','white','Name',filename)
+% imagesc(log10(ag412))
+% colormap jet
+% caxis([log10(min(ag412(:))),log10(max(ag412(:)))])  
+% hcb = colorbar('southoutside');
+% y = get(hcb,'XTick');
+% % [xmin,xmax] = caxis;
+% x = 10.^y;
+% set(hcb,'XTick',log10(x));
+% set(hcb,'XTickLabel',x)
 %%
 plusdegress = 0.5;
 latlimplot = [min(latitude(:))-.5*plusdegress max(latitude(:))+.5*plusdegress];
 lonlimplot = [min(longitude(:))-plusdegress max(longitude(:))+plusdegress];
 
-figure('Color','white')
+figure('Color','white','Name',filename)
 % ax = worldmap([52 75],[170 -120]);
 ax = worldmap(latlimplot,lonlimplot);
 
@@ -132,14 +154,14 @@ geoshow(ax,'worldrivers.shp', 'Color', 'blue')
 geoshow(ax,latitude,longitude,log10(ag412),'DisplayType','surface',...
       'ZData',zeros(size(ag412)),'CData',log10(ag412))
 colormap jet
-% caxis([log10(min(ag412(:))),log10(max(ag412(:)))]) 
-caxis([log10(0.00030006314),log10(2.968400001526)]) % from colorbar in SeaDAS
+caxis([log10(cmin),log10(cmax)])
 hcb = colorbar('southoutside');
 % set(get(hcb,'Xlabel'),'String','Chl-a [mg m\^-3]')
 fs = 11;
 set(hcb,'fontsize',fs,'Location','southoutside')
 set(hcb,'Position',[.2 .15 .6 .05])
-title(hcb,'ag\_412 [m\^-1]','FontSize',fs)
+title('ag\_412 (Matlab)','FontSize',fs)
+title(hcb,'a_{CDOM} (m\^-1)','FontSize',fs)
 % set(gca, 'Units', 'normalized', 'Position', [0 0.1 1 1])
 y = get(hcb,'XTick');
 % [xmin,xmax] = caxis;
@@ -150,8 +172,8 @@ set(hcb,'XTickLabel',x)
 %% Comparison
 disp('Percentage of error:')
 disp(nanmean(ag412(:)-ag_412_mlrc(:))/(nanmin(ag412(:)-nanmax(ag412(:)))))
-figure,histogram(ag412(:)),title('ag412')
-figure,histogram(ag_412_mlrc(:)),title('ag\_412\_mlrc')
+figure('Color','white','Name',filename),histogram(ag412(:)),title('ag\_412 (Matlab)','FontSize',fs)
+figure('Color','white','Name',filename),histogram(ag_412_mlrc(:)),title('ag\_412\_mlrc (SeaDAS)','FontSize',fs)
 xlim([0 1])
 %%
 % Checking if they are real
