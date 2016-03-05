dfe4d808d70f271dc790fadccaaf015d17cc624b
Default Seadas User <seadas-user@localhost>
2016-03-05 14:37:29 -0500

    Combining all the scripts for matchups in just one file...

---
+++ Research/LANDSAT8/InSituLANDSAT_Main.m
@@ -0,0 +1,13 @@
+% Script to find Landsat 8 matchups based on in situ data
+
+%% Create WRS Structure
+
+%% Load in situ data
+
+%% Find path and row for in situ data
+
+%% Find Matchups
+
+%% Plot Matchups
+
+%% Plot retrieved vs in situ
