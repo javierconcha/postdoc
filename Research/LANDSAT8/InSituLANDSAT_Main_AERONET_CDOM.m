1a9066f07dd259604b1dd3b2cd9fdb43c89e31c2
Default Seadas User <seadas-user@localhost>
2016-08-30 10:25:43 -0400

    Before modify plot for SPIE San Diego....

--- Research/LANDSAT8/InSituLANDSAT_Main_AERONET_CDOM.m
+++ Research/LANDSAT8/InSituLANDSAT_Main_AERONET_CDOM.m
@@ -4,18 +4,18 @@ addpath('/Users/jconchas/Documents/Research/')
 cd '/Users/jconchas/Documents/Research/LANDSAT8';
 %% First load data generated from InSituLANDSAT_Main_AERONETOC_Rrs.m
 clear
-load('L8Matchups_AERONET_Rrs.mat','MatchupRealFilt')
+load('L8Matchups_AERONET_Rrs.mat','MatchupRealFilt','InSitu')
 
-% L2ext = {'_L2n1.nc','_L2n2.nc','_L2n2SWIR5x5.nc'};
+L2ext = {'_L2n1.nc','_L2n2.nc','_L2n2SWIR5x5.nc'};
 
-L2ext = {'_L2n2.nc'};
+% L2ext = {'_L2n2.nc'};
 
 count0 = 0;
 for idx0 = 1:size(L2ext,2)
       cond_ACpar = strcmp({MatchupRealFilt.scene_ACpar}',char(L2ext(idx0)));
       
       Matchup_used = MatchupRealFilt(cond_ACpar);
-      
+      %%
       for idx1 = 1:size(Matchup_used,2)
 
             if ~isempty(Matchup_used(idx1).Rrs_443_filt_mean)&&...
@@ -27,14 +27,14 @@ for idx0 = 1:size(L2ext,2)
                               ~isnan(Matchup_used(idx1).Rrs_561_filt_mean)&&...
                               ~isnan(Matchup_used(idx1).Rrs_555_insitu)
                         
-                        count0 = count0+1;
-                        
                         scene_id = Matchup_used(idx1).scene_id;
                         doy = str2double(scene_id(14:16));
                         
+                        count0 = count0 + 1;
+                        
                         % In Situ CDOM
                         % [DOC,ag412] = doc_algorithm_ag412_daily( Rrs_b,Rrs_g,doy,wl_g)
-                        [DOC_ins(count0),ag412_ins(count0)] = doc_algorithm_ag412_daily(...
+                        [DOC_ins,ag412_ins] = doc_algorithm_ag412_daily(...
                               Matchup_used(idx1).Rrs_443_insitu,...
                               Matchup_used(idx1).Rrs_555_insitu,...
                               doy,...
@@ -42,20 +42,56 @@ for idx0 = 1:size(L2ext,2)
                         
                         % Satellite CDOM
                         % [DOC,ag412] = doc_algorithm_ag412_daily( Rrs_b,Rrs_g,doy,wl_g)
-                        [DOC_sat(count0),ag412_sat(count0)] = doc_algorithm_ag412_daily(...
+                        [DOC_sat,ag412_sat] = doc_algorithm_ag412_daily(...
                               Matchup_used(idx1).Rrs_443_filt_mean,...
                               Matchup_used(idx1).Rrs_561_filt_mean,...
                               doy,...
                               '561');
+                        
+                        Matchup_l2prod(count0).scene_id             = Matchup_used(idx1).scene_id;
+                        Matchup_l2prod(count0).scene_time           = Matchup_used(idx1).scene_time;
+                        Matchup_l2prod(count0).scene_ACpar          = Matchup_used(idx1).scene_ACpar;
+                        Matchup_l2prod(count0).Rrs_443_filt_mean    = Matchup_used(idx1).Rrs_443_filt_mean;
+                        Matchup_l2prod(count0).Rrs_443_insitu       = Matchup_used(idx1).Rrs_443_insitu;
+                        Matchup_l2prod(count0).Rrs_443_insitu_time  = Matchup_used(idx1).Rrs_443_insitu_time;
+                        Matchup_l2prod(count0).Rrs_443_insitu_index = Matchup_used(idx1).Rrs_443_insitu_index;
+                        Matchup_l2prod(count0).Rrs_482_filt_mean    = Matchup_used(idx1).Rrs_482_filt_mean;
+                        Matchup_l2prod(count0).Rrs_486_insitu       = Matchup_used(idx1).Rrs_486_insitu;
+                        Matchup_l2prod(count0).Rrs_486_insitu_time  = Matchup_used(idx1).Rrs_486_insitu_time;
+                        Matchup_l2prod(count0).Rrs_486_insitu_index = Matchup_used(idx1).Rrs_486_insitu_index;
+                        Matchup_l2prod(count0).Rrs_561_filt_mean    = Matchup_used(idx1).Rrs_561_filt_mean;
+                        Matchup_l2prod(count0).Rrs_555_insitu       = Matchup_used(idx1).Rrs_555_insitu;
+                        Matchup_l2prod(count0).Rrs_555_insitu_time  = Matchup_used(idx1).Rrs_555_insitu_time;
+                        Matchup_l2prod(count0).Rrs_555_insitu_index = Matchup_used(idx1).Rrs_555_insitu_index;
+                        Matchup_l2prod(count0).Rrs_655_filt_mean    = Matchup_used(idx1).Rrs_655_filt_mean;
+                        Matchup_l2prod(count0).Rrs_665_insitu       = Matchup_used(idx1).Rrs_665_insitu;
+                        Matchup_l2prod(count0).Rrs_665_insitu_time  = Matchup_used(idx1).Rrs_665_insitu_time;
+                        Matchup_l2prod(count0).Rrs_665_insitu_index = Matchup_used(idx1).Rrs_665_insitu_index;
+                        
+                        Matchup_l2prod(count0).DOC_ins = DOC_ins;
+                        Matchup_l2prod(count0).DOC_sat = DOC_sat;
+                        Matchup_l2prod(count0).ag412_ins = ag412_ins;
+                        Matchup_l2prod(count0).ag412_sat = ag412_sat;
                   end
             end
       end
       
       %% Plot ag412
-      cond1 = ag412_ins >=0 & ~isnan(ag412_ins) & ag412_sat >=0 & ~isnan(ag412_sat);
       
-      ag412_ins_used = ag412_ins(cond1);
-      ag412_sat_used = ag412_sat(cond1);
+      cond_ACpar2 = strcmp({Matchup_l2prod.scene_ACpar}',char(L2ext(idx0)));
+      
+      temp_ag412_ins = [Matchup_l2prod(cond_ACpar2).ag412_ins];
+      temp_ag412_sat = [Matchup_l2prod(cond_ACpar2).ag412_sat];
+      
+      cond1 = temp_ag412_ins >=0 & ~isnan(temp_ag412_ins) &...
+            temp_ag412_sat >=0 & ~isnan(temp_ag412_sat);
+      
+      cond2 = temp_ag412_ins <= 0.4;
+      
+      ag412_ins_used = temp_ag412_ins(cond1&cond2);
+      ag412_sat_used = temp_ag412_sat(cond1&cond2);
+      
+%       clear temp_ag412_ins temp_ag412_sat
       
       fs = 16;
       h = figure('Color','white','DefaultAxesFontSize',fs,'Name',char(L2ext(idx0)));
@@ -88,13 +124,15 @@ for idx0 = 1:size(L2ext,2)
       ax = gca;
       ax.XTick =ax.YTick;
       
-      % Statistics
+      %% Statistics
       C_insitu_temp = ag412_ins_used;
       
       C_alg_temp = ag412_sat_used;
       
       C_insitu = C_insitu_temp(C_alg_temp>0);
       C_alg = C_alg_temp(C_alg_temp>0);
+      
+      clear C_alg_temp C_alg_temp
       
       if size(C_insitu,2) == size(C_alg,2)
             N = size(C_insitu,2);
@@ -122,7 +160,7 @@ for idx0 = 1:size(L2ext,2)
       Q1 = prctile(ratio_alg_insitu,25);
       SIQR = (Q3-Q1)/2; % Semi-interquartile range
       
-      % RMA regression and r-squared (or coefficient of determination)
+      %% RMA regression and r-squared (or coefficient of determination)
       regressiontype = 'RMA';
       
       if strcmp(regressiontype,'OLS')
